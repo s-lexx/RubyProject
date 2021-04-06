456a4b315db7d63e589fdd53ef7ec43121113b39
s-lexx <s-lexx30@yandex.ru>
2021-04-07 00:33:02 +0300

    Refactoring

--- methods.rb
+++ methods.rb
@@ -1,81 +1,63 @@
-def get_letters
-  # реализация метода
-  slovo = ARGV[0]
-
-  #if (Gem.win_platform? && ARGV[0])
-  #	slovo = slovo.encode(ARGV[0].encoding, 'cp1251').encode('UTF-8').chomp
- # end
-
-  if (slovo == nil || slovo == "")
-  	abort "Вы не ввели слово для игры"
+def get_letters # проверка и получение слова от пользователя
+  words = ARGV[0]
+  if(words == nil || words == "")
+  	 abort "Вы не загадали слово для игры"
   end
-  return slovo.encode('UTF-8').split('')
+  return words.encode('UTF-8').split('')
 end
 
-def get_user_input
-	letter = ''
-
-	while letter == '' do
-		letter = STDIN.gets.encode('UTF-8').chomp
-	end
-
-	return letter
+def get_user_input # пользователь угадывает букву
+	 letter = ''
+	 while letter == '' do
+		 letter = STDIN.gets.encode('UTF-8').chomp
+	 end
+	letter
 end
 
-def check_result(user_input, letters, good_letters, bad_letters)
-
-puts "введенная буква:" + user_input
-	if(good_letters.include?(user_input) ||
-	bad_letters.include?(user_input))
-	return 0
-	end
-
+def check_result(user_input, letters, good_letters, bad_letters) # проверка результата хода игрока
+		if(good_letters.include?(user_input) ||
+			 bad_letters.include?(user_input))
+		return 0
+		end
 		if letters.include? user_input
-		good_letters << user_input
-
+			 good_letters << user_input
 	 	if letters.uniq.size == good_letters.size
 		return 1
-	    else
+	  else
 		return 0	
 		end
-	else
-		bad_letters << user_input 
-	return -1
-	end
+		else
+		  bad_letters << user_input 
+		return -1
+		end
 end	
 
-def get_word_for_print(letters, good_letters)
-result= ''
-
-	for letters in letters do
-		if good_letters.include? letters
-			result += letters + ' '
-		else
-			result += '__'
-		end		
-	end
-	return result
+def get_word_for_print(letters, good_letters) #прорисовка прогресса угадывания слова
+		result = ''
+		letters.each do |letters|  										
+				  if (good_letters.include? letters)
+					result += letters + ' '
+					else
+					result += '__'
+					end	
+				end		
+	result
 end
 
 def print_status(letters, good_letters, bad_letters, errors)
-	puts "\nСлово: " + get_word_for_print(letters, good_letters)
-
-	puts "Ошибки (#{errors}): #{bad_letters.join(", ")}"
-
-	if errors >= 7 
-		puts " Вы проиграли  :("
-	else
-		if letters.uniq.size == good_letters.size
-			puts "Поздравляем! Вы выйграли!\n\n"
-		else
-			puts "У вас осталось попыток: " + (7-errors).to_s
-		end			
+	 puts "\nСлово: " + get_word_for_print(letters, good_letters)
+	 puts "Ошибки (#{errors}): #{bad_letters.join(", ")}"
+	 if errors >= 7 
+		 puts " Вы проиграли  :("
+	 else
+		 if letters.uniq.size == good_letters.size
+			 puts "Поздравляем! Вы выйграли!\n\n"
+		 else
+			 puts "У вас осталось попыток: " + (7-errors).to_s
+		 end			
 	end	
 end	
 
 def cls
-
-system "clear" or system 	"cls"
-
+system "clear" or system "cls"
 end
-
---
+++ pendant.rb
@@ -0,0 +1,29 @@
+require_relative 'methods'
+if (Gem.win_platform?) # Блок кода для устранения проблемы c кириллицей на платформе Windows
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+  [STDIN, STDOUT].each do |io|
+   io.set_encoding(Encoding.default_external, Encoding.default_internal)
+   end
+end
+
+errors = 0 # начальная инициализация переменных
+good_letters = []
+bad_letters = []
+cls
+letters = get_letters
+
+while errors < 7 do  #основной блок 
+  print_status(letters, good_letters, bad_letters, errors)
+  puts "Введите следующую букву" 
+  user_input = get_user_input
+  result = check_result(user_input, letters, good_letters, bad_letters)
+  if (result == -1)
+  	errors += 1
+  elsif (result == 1)
+    break		
+  end  	
+end
+
+print_status(letters, good_letters, bad_letters, errors) # вывод результата игры
+ 
