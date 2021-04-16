d7cefa0a8ce5ccc2fdeee2fdd6d6abcb2d30a6b7
s-lexx <s-lexx30@yandex.ru>
2021-04-17 00:46:45 +0300

    refactored

--- develop/methods.rb
+++ develop/methods.rb
@@ -1,6 +1,7 @@
 def get_letters # проверка и получение слова от пользователя
   words = ARGV[0]
   abort "Вы не загадали слово для игры" if (words == nil) || (words == "")
+  
   words.encode('UTF-8').split('')
 end
 
@@ -16,24 +17,25 @@ end
 
 def check_input(user_input, letters, good_letters, bad_letters) # проверка результата хода игрока
   return 0 if good_letters.include?(user_input) || bad_letters.include?(user_input)
-  if letters.include?(user_input) || (user_input == 'е' && letters.include?('ё')) || (user_input == 'ё' && letters.include?('е')) ||
-     (user_input == 'и' && letters.include?('й')) || (user_input == 'й' && letters.include?('и'))
-     if user_input == 'е'
-        good_letters << 'ё'
-     end
-     if user_input == 'ё'
-        good_letters << 'е'
-     end
-     if user_input == 'и'
-        good_letters << 'й'
-     end
-     if user_input == 'й'
-        good_letters << 'и'
-     end
-    
-     good_letters << user_input
 
-     (letters - good_letters).empty? ? 1 : 0
+  if letters.include?(user_input)                ||
+    (user_input == 'е' && letters.include?('ё')) ||
+    (user_input == 'ё' && letters.include?('е')) ||
+    (user_input == 'и' && letters.include?('й')) ||
+    (user_input == 'й' && letters.include?('и'))
+     
+    good_letters << #user_input
+      case user_input
+      when 'е'; 'ё'
+      when 'ё'; 'е'
+      when 'и'; 'й'
+      when 'й'; 'и'
+      #else user_input
+      end
+      
+    good_letters << user_input
+
+    (letters - good_letters).empty? ? 1 : 0
   else
     bad_letters << user_input
 
@@ -53,13 +55,11 @@ def print_status(letters, good_letters, bad_letters, errors)
   puts "Ошибки (#{errors}): #{bad_letters.join(", ")}"
 
   if errors >= 7 
-    puts " Вы проиграли  :(\n\n"
+    puts "Вы проиграли :(\n\n"
+  elsif (letters - good_letters).empty?
+    puts "Поздравляем! Вы выиграли!\n\n"
   else
-    if (letters - good_letters).empty?
-      puts "Поздравляем! Вы выиграли!\n\n"
-    else
-      puts "У вас осталось попыток: #{7 - errors}"
-    end
+    puts "У вас осталось попыток: #{7 - errors}"
   end
 end
 
--- develop/pendant.rb
+++ develop/pendant.rb
@@ -2,9 +2,7 @@ require_relative 'methods'
 if (Gem.win_platform?) # Блок кода для устранения проблемы c кириллицей на платформе Windows
   Encoding.default_external = Encoding.find(Encoding.locale_charmap)
   Encoding.default_internal = __ENCODING__
-  [STDIN, STDOUT].each do |io|
-   io.set_encoding(Encoding.default_external, Encoding.default_internal)
-   end
+  [STDIN, STDOUT].each { |io| io.set_encoding(Encoding.default_external, Encoding.default_internal) }
 end
 
 errors = 0 # начальная инициализация переменных
@@ -16,13 +14,15 @@ letters = get_letters
 while errors < 7 do  #основной блок 
   print_status(letters, good_letters, bad_letters, errors)
   puts "Введите следующую букву" 
+  
   user_input = get_user_input
   result = check_input(user_input, letters, good_letters, bad_letters)
+  
   if (result == -1)
-  	errors += 1
+    errors += 1
   elsif (result == 1)
-    break		
-  end  	
+    break
+  end
 end
 
 print_status(letters, good_letters, bad_letters, errors) # вывод результата игры
